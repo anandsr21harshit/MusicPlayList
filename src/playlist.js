4788e9f6c8bd97d53532215fefd23279cd5aaf16
Harshit Anand <78558235+anandsr21harshit@users.noreply.github.com>
2021-09-05 14:13:38 +0530

    pic links added

--- src/playlist.js
+++ src/playlist.js
@@ -1,20 +1,70 @@
 const playList = {
   Romantic: [
-    { name: "Na Tum Jaano Na Hum", singer: "Lucky Ali" },
-    { name: "Tere Naam", singer: "Udit Narayan" },
-    { name: "Sun Raha Hai Na Tu", singer: "Shreya Ghosal" },
-    { name: "Chand Sifarish", singer: "Shaan" }
+    {
+      name: "Na Tum Jaano Na Hum",
+      singer: "Lucky Ali",
+      pic:
+        "https://a10.gaanacdn.com/images/albums/43/25343/crop_480x480_25343.jpg"
+    },
+    {
+      name: "Tere Naam",
+      singer: "Udit Narayan",
+      pic: "https://a10.gaanacdn.com/images/albums/11/111/crop_480x480_111.jpg"
+    },
+    {
+      name: "Sun Raha Hai Na Tu",
+      singer: "Shreya Ghosal",
+      pic:
+        "https://a10.gaanacdn.com/gn_img/albums/d41WjnWPLq/41Wjz1LOWP/size_l.webp"
+    },
+    {
+      name: "Chand Sifarish",
+      singer: "Shaan",
+      pic:
+        "https://a10.gaanacdn.com/gn_img/albums/XzVWRyL3dq/zVWRr6kWdq/size_l.webp"
+    }
   ],
   JourneySong: [
-    { name: "The Journey Song", singer: "Anupam Roy" },
-    { name: "Khaabon Ke Parindey", singer: "Mohit Chauhan" },
-    { name: "Ilhai", singer: "Arijit Sing" },
-    { name: "Safarnam", singer: "Lucky Ali" }
+    {
+      name: "The Journey Song",
+      singer: "Anupam Roy",
+      pic: "https://c.saavncdn.com/009/Piku-Hindi-2018-20180516-500x500.jpg"
+    },
+    {
+      name: "Khaabon Ke Parindey",
+      singer: "Mohit Chauhan",
+      pic:
+        "https://c.saavncdn.com/787/Zindagi-Na-Milegi-Dobara-2011-500x500.jpg"
+    },
+    {
+      name: "Ilhai",
+      singer: "Arijit Sing",
+      pic: "https://c.saavncdn.com/440/Yeh-Jawaani-Hai-Deewani-2013-500x500.jpg"
+    },
+    {
+      name: "Safarnam",
+      singer: "Lucky Ali",
+      pic:
+        "https://a10.gaanacdn.com/gn_img/albums/10q3ZR1352/0q3ZzPm035/size_l.webp"
+    }
   ],
   GymSong: [
-    { name: "Kar Har Maida Fateh", singer: "Sukhwinder Singh" },
-    { name: "Ziddi Dil", singer: "Vishal Dadlani" },
-    { name: "Zinda", singer: "Siddharth Mahadevan" }
+    {
+      name: "Kar Har Maida Fateh",
+      singer: "Sukhwinder Singh",
+      pic: "https://c.saavncdn.com/319/Sanju-Hindi-2018-20180629-500x500.jpg"
+    },
+    {
+      name: "Ziddi Dil",
+      singer: "Vishal Dadlani",
+      pic: "https://c.saavncdn.com/988/Mary-Kom-Hindi-2018-20180516-500x500.jpg"
+    },
+    {
+      name: "Zinda",
+      singer: "Siddharth Mahadevan",
+      pic:
+        "https://c.saavncdn.com/575/Bhaag-Milkha-Bhaag-Hindi-2013-20190607111645-500x500.jpg"
+    }
   ]
 };
 
